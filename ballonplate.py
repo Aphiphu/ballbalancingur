199f6693f431f9f580f4e29be6d68790c06ed99d
Aphiphu <70745633+Aphiphu@users.noreply.github.com>
2023-07-06 18:55:13 +0700

    Update ballonplate.py

--- ballonplate.py
+++ ballonplate.py
@@ -2,9 +2,9 @@ import socket
 import time
 import cv2
 import csv
-import math
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # the IP address of the UR arm
 ROBOT_IP = '10.10.0.14'
@@ -15,20 +15,20 @@ MOVE_TIME = 0.3
 # port used by robotiq gripper
 GRIPPER_PORT = 63352
 # the starting pose of the gripper (in [m, m, m, rad, rad, rad])
-HOME_POSE = [0.0662, -0.4362, 0.5636, 0.0, 0.0, 0.0]# the starting pose of the gripper (in [m, m, m, rad, rad, rad])
-CHANGE_POSE = HOME_POSE.copy()
-
-# Constants for plate position
+HOME_POSE = [0.0662, -0.4362, 0.5636, 0.0, 0.0, 0.0]
+# the starting pose of the gripper (in [m, m, m, rad, rad, rad])
+CHANGE_POSE = [0.0662, -0.4362, 0.5636, 0.0, 0.0, 0.0]
+# the middle point of the box
+count =0 
 set_y = 220
 set_x = 250
 max_y = 475
 min_y = 1.5
 max_x = 422
 min_x = 12
-# array to save change in angle
+# max angle deflection (in rad) 
 y_rad_change=[0,0]
 # PID controller
-count = 0 
 pixel_1m_y = (max_y-min_y)/0.2
 pixel_1m_x = (max_x-min_x)/0.13
 Kc_y = 0.8/pixel_1m_y
@@ -42,15 +42,16 @@ time_step = 0.03
 integral = [0.0,0.0]
 derivative = [0.0,0.0]
 #Store variable for ploting
-t=np.array([])
+t = np.array([])
 xs = np.array([])
 ys = np.array([])
 start=[]
 angle_stored_x = []
 angle_stored_y = []
+x_dot0 =0
 e = np.array([])
-# pandas dataframe for collecting data
-df=pd.DataFrame()
+SP = np.ones(len(t)) * 0
+
 # pixel offset (in pixels)
 pixel_off_x = 10
 pixel_off_y = 10
@@ -60,10 +61,9 @@ BUFFER_SIZE = 255
 class RobotCommander():
     
     def __init__(self):
-        #set arm and gripper connection variables
         self._arm_connection = None
         self._gripper_connection = None
-        # run establishing functions for arm and gripper
+        
         self._establish_arm_connection()
         self._establish_gripper_connection()
         
@@ -80,9 +80,20 @@ class RobotCommander():
     def _establish_gripper_connection(self):
         self._gripper_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._gripper_connection.connect((ROBOT_IP, GRIPPER_PORT))
+        # recv = self._gripper_connection.recv(BUFFER_SIZE)
+        # if recv:
+        #         print('succesfully connected to the gripper')
+        #         self.grip_release()
+        # else:
+        #         print('failed to connect to the gripper')
         
     def movel(self, target_pose, a=1.2, v=0.25, t=0, r=0):
         cmd_str = f"movel(p{target_pose},{a},{v},{t},{r})\n"
+        print(cmd_str)
+        self._arm_connection.send(bytes(cmd_str, "UTF-8"))
+
+    def movej(self, target_joint_params, a=1.2, v=0.25, t=0, r=0):
+        cmd_str = f"movel({target_joint_params},{a},{v},{t},{r})\n"
         print(cmd_str)
         self._arm_connection.send(bytes(cmd_str, "UTF-8"))
         
@@ -110,16 +121,15 @@ def calc_angle(pos, i):
     # PID eq
     angle_x = (-Kc_x *error[0] - Kd_x*derivative[0] - Ki_x*integral[0])/1.5
     angle_y = (-Kc_y *error[1] - Kd_y*derivative[1] - Ki_y*integral[1])/1.5
-    # Limit movement of arm (in rad) 
-    max_angle = math.radians(10)
-    if angle_x > max_angle:
-        angle_x = max_angle
-    if angle_x < -max_angle:
-        angle_x = -max_angle
-    if angle_y > max_angle:
-        angle_y = max_angle
-    if angle_y < -max_angle:
-        angle_y = -max_angle
+    # Limit movement of arm
+    if angle_x > 0.174533:
+        angle_x = 0.174533
+    if angle_x < -0.174533:
+        angle_x = -0.174533
+    if angle_y > 0.174533:
+        angle_y = 0.174533
+    if angle_y < -0.174533:
+        angle_y = -0.174533
     # on the spot checking
     print("Error", error)
     print("Derivative", derivative)
@@ -162,8 +172,14 @@ if __name__ == '__main__':
             cv2.circle(frame,(set_x,set_y),20,(255,255,255),2)
             cv2.circle(frame,center,radius,(0,255,0),2)
             cv2.circle(frame,center,radius=5, color=(0, 0, 255), thickness=-1)
-            print(f"x:{x},y:{y}")            
+            print(f"x:{x},y:{y}")
+
+            # if (abs(x-set_x)>pixel_off) and (x<590) and (x>50) :
+            
+
+            
             rad_change = calc_angle([x,y], count)
+                
             count+=1
             print("Detected count:", count)
             CHANGE_POSE[3]=-rad_change[0]
@@ -171,17 +187,37 @@ if __name__ == '__main__':
             CHANGE_POSE[4]=rad_change[1]
             if (abs(y-set_y)<pixel_off_y): CHANGE_POSE[4]=0
             robot_commander.movel(CHANGE_POSE, t=MOVE_TIME)
+            #time.sleep(MOVE_TIME)
+            # elif (abs(x-set_x)<pixel_off):
+            #     robot_commander.movel(HOME_POSE, t=MOVE_TIME)
+            #     #time.sleep(MOVE_TIME)
+                
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            ['cur_x','cur_y','error','derivative','integral','angle_x','angle_y','time']
-            df['cur_x']=xs
-            df['cur_y']=ys
-            df['error']=e
-            df['derivative']=derivative
-            df['integral']=integral
-            df['angle_x']=angle_stored_x
-            df['angle_y']=angle_stored_y
-            df['time']=time
             break
     cap.release()
     cv2.destroyAllWindows()
+    set_x_plot = np.ones(len(t)) * set_x
+    set_y_plot = np.ones(len(t)) * set_y
+
+    plt.subplot(2,2,1)
+    plt.plot(t,xs,'r-',linewidth = 0.5,label = "x coord. (pixel)")
+    plt.plot(t,set_x_plot,'k-',linewidth=1,label="x set point")
+    plt.xlabel = "Time(s)"
+    
+
+    plt.subplot(2,2,2)
+    plt.plot(t,ys,'r-',linewidth = 0.5,label = "y coord. (pixel)")
+    plt.plot(t,set_y_plot,'k-',linewidth=1,label="x set point")
+    plt.xlabel = "Time(s)"
+
+    plt.subplot(2,2,3)
+    plt.plot(t,angle_stored_x,'r-',linewidth = 0.5,label = "Angle x (rad)")
+    plt.xlabel = "Time(s)"
+
+    plt.subplot(2,2,4)
+    plt.plot(t,angle_stored_y,'r-',linewidth = 0.5,label = "Angle y (rad)")
+    plt.xlabel = "Time(s)"
+
+    plt.legend(loc="best")
+    plt.show()
