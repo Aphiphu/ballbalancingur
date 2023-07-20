7c50a994e2df03330b2281962cf2d9ad8539ffb3
Aphiphu <70745633+Aphiphu@users.noreply.github.com>
2023-07-20 18:49:18 +0700

    Update ballonplate.py

--- ballonplate.py
+++ ballonplate.py
@@ -5,46 +5,45 @@ import csv
 import math
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # the IP address of the UR arm
 ROBOT_IP = '10.10.0.14'
 # port used by UR arm
 ARM_PORT = 30003
 # arbeitely chosen move time (in s)
-MOVE_TIME = 0.3
+MOVE_TIME = 0.29
 # port used by robotiq gripper
 GRIPPER_PORT = 63352
 # the starting pose of the gripper (in [m, m, m, rad, rad, rad])
 HOME_POSE = [0.0662, -0.4362, 0.5636, 0.0, 0.0, 0.0]# the starting pose of the gripper (in [m, m, m, rad, rad, rad])
-CHANGE_POSE = HOME_POSE.copy()
-# Constants for plate position in pixels
-set_y = 220
-set_x = 250
-max_y = 475
-min_y = 1.5
-max_x = 422
-min_x = 12
-# Plate size in cm
-plate_y= 20
-plate_X= 13
+CHANGE_POSE = [0.0662, -0.4362, 0.5636, 0.0, 0.0, 0.0]
+
+# Constants for plate position
+set_y = 180
+set_x = 320
+max_y = 470
+min_y = 15
+max_x = 531
+min_x = 170
 # array to save change in angle
 y_rad_change=[0,0]
 # PID controller
 count = 0 
 pixel_1m_y = (max_y-min_y)/0.2
 pixel_1m_x = (max_x-min_x)/0.13
-Kc_y = 0.8/pixel_1m_y
-Ki_y = 0.15/pixel_1m_y
-Kd_y = 2.2/pixel_1m_y
+Kc_y = 2.1/pixel_1m_y
+Ki_y = 0.5/pixel_1m_y
+Kd_y = 3.5/pixel_1m_y
 
-Kc_x = 0.8/pixel_1m_x
-Ki_x = 0.15/pixel_1m_x
-Kd_x = 2.2/pixel_1m_x
-time_step = 0.03
+Kc_x = 2.1/pixel_1m_x
+Ki_x = 0.5/pixel_1m_x
+Kd_x = 3.5/pixel_1m_x
+time_step = 0.04
 integral = [0.0,0.0]
 derivative = [0.0,0.0]
 #Store variable for ploting
-radius_Arr=[]
+t0 = time.time()
 t=np.array([])
 xs = np.array([])
 ys = np.array([])
@@ -54,9 +53,9 @@ angle_stored_y = []
 e = np.array([])
 # pandas dataframe for collecting data
 df=pd.DataFrame()
-#Pixel to MM conversion
-PIXEL_TO_MM=1
-
+# pixel offset (in pixels)
+pixel_off_x = 20
+pixel_off_y = 20
 # the buffer size
 BUFFER_SIZE = 255
 
@@ -96,8 +95,8 @@ class RobotCommander():
         self._gripper_connection.send(b'SET POS 0\n')
 
 def calc_angle(pos, i):
-    global integral, derivative, time_step, xs, ys, t, e 
-    t = np.append(t,time.time()-t0)
+    global integral, derivative, time_step, xs, ys,t0, t, e 
+    t = np.append(t,(time.time()-t0))
     x0 = pos[0]
     y0 = pos[1]
     xs = np.append(xs,x0)
@@ -109,15 +108,24 @@ def calc_angle(pos, i):
         derivative[1] = (ys[i]-ys[i-1])/ time_step
     integral[0] += error[0]*time_step
     integral[1] += error[1]*time_step
+    
     # PID eq
     angle_x = (-Kc_x *error[0] - Kd_x*derivative[0] - Ki_x*integral[0])/1.5
     angle_y = (-Kc_y *error[1] - Kd_y*derivative[1] - Ki_y*integral[1])/1.5
     # Limit movement of arm (in rad) 
-    max_angle = math.radians(10)
-    if angle_x >  max_angle: angle_x =  max_angle
-    if angle_x < -max_angle: angle_x = -max_angle
-    if angle_y >  max_angle: angle_y =  max_angle
-    if angle_y < -max_angle: angle_y = -max_angle
+    max_angle = math.radians(3.2)
+    if angle_x > max_angle:
+        angle_x = max_angle
+    if angle_x < -max_angle:
+        angle_x = -max_angle
+    if angle_y > max_angle:
+        angle_y = max_angle
+    if angle_y < -max_angle:
+        angle_y = -max_angle
+    # on the spot checking
+    print("Error", error)
+    print("Derivative", derivative)
+    print("Integral", integral)
     angle_stored_x.append(angle_x)
     angle_stored_y.append(angle_y)
     return angle_x,angle_y
@@ -147,19 +155,16 @@ if __name__ == '__main__':
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.circle(frame,(set_x,set_y),20,(255,255,255),2)
+        
         if len(contours) > 0:
-            t0 = time.time()
+            #t0 = time.time()
             c = max(contours, key=cv2.contourArea)
             (x,y),radius = cv2.minEnclosingCircle(c)
             center = (int(x),int(y))
             radius = int(radius)
-            radius_Arr.append(radius)
             cv2.circle(frame,(set_x,set_y),20,(255,255,255),2)
             cv2.circle(frame,center,radius,(0,255,0),2)
-            # pixel offset (in pixels)
-            pixel_off_x = radius/3
-            pixel_off_y = radius/3
-            cv2.circle(frame,center,radius=5, color=(0, 0, 255), thickness=-1)
+            cv2.circle(frame,center,radius=int((pixel_off_x+pixel_off_y)/2), color=(0, 0, 255), thickness=-1)
             print(f"x:{x},y:{y}")            
             rad_change = calc_angle([x,y], count)
             count+=1
@@ -168,33 +173,40 @@ if __name__ == '__main__':
             if (abs(x-set_x)<pixel_off_x): CHANGE_POSE[3]=0
             CHANGE_POSE[4]=rad_change[1]
             if (abs(y-set_y)<pixel_off_y): CHANGE_POSE[4]=0
+            # time.sleep(0.03)
+           
             robot_commander.movel(CHANGE_POSE, t=MOVE_TIME)
-        else:
-             robot_commander.movel(HOME_POSE, t=MOVE_TIME)
+            time.sleep(time_step)
         cv2.imshow("Frame", frame)
+            #if (abs(xs[-1]-set_x)<pixel_off_x) and (abs(ys[-1]-set_y)<pixel_off_y):
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            df['radius']=radius_Arr
-            df['cur_x']=xs
-            df['cur_y']=ys
-            e_X=[]; e_Y=[]
-            for i in range(len(e)): e_X.append(e[i])  if i%2==0 else e_Y.append(e[i])  
-            df['error_X']=e_X
-            df['error_Y']=e_Y
-            df['derivative_X']=derivative[0]
-            df['derivative_y']=derivative[1]
-            df['integral_x']=integral[0]
-            df['integral_y']=integral[1]
-            df['angle_x']=angle_stored_x
-            df['angle_y']=angle_stored_y
-            delta_angle_x=angle_stored_x.copy()
-            delta_angle_y=angle_stored_y.copy()
-            for i in range(1, len(delta_angle_x)):
-                delta_angle_x[i]=delta_angle_x[i]-delta_angle_x[i-1]
-                delta_angle_y[i]=delta_angle_y[i]-delta_angle_y[i-1]
-            df['delta_angle_x']= delta_angle_x
-            df['delta_angle_y']= delta_angle_y
-            df['time']=t
-            df.to_csv('balloutput.csv', encoding='utf-8')
+            # ['cur_x','cur_y','error','derivative','integral','angle_x','angle_y','time']
+            # df['cur_x']=xs
+            # df['cur_y']=ys
+            # df['error']=e
+            # df['derivative']=derivative
+            # df['integral']=integral
+            # df['angle_x']=angle_stored_x
+            # df['angle_y']=angle_stored_y
+            # df['time']=time
+            # df.to_csv("C:/Users/User3/Desktop/ball_plate.csv",index=False)
             break
     cap.release()
     cv2.destroyAllWindows()
+    data = {"x":xs,"y":ys,"angle_x":angle_stored_x,"angle_y":angle_stored_y,"Time":t}
+    time_data = {"Time":t}
+    df = pd.DataFrame(data)
+    df.to_csv("C:/Users/User3/Desktop/kc_1_corner1.csv",index=False)
+    #print(df.head())
+    plot_set_x = np.ones(len(t)) *set_x
+    plot_set_y = np.ones(len(t)) *set_y
+    df2 = pd.DataFrame(time_data)
+    print(df2.head(n=20))
+    plt.subplot(2,1,1)
+    plt.plot(t,xs)
+    plt.plot(t,plot_set_x)
+    plt.subplot(2,1,2)
+    plt.plot(t,ys)
+    plt.plot(t,plot_set_y)
+    plt.legend()
+    plt.show()
